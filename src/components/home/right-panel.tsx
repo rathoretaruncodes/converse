759a6f3a663b608b18a2ff3378575eb015c70b81
rathoretaruncodes <rathoretaruncodes@gmail.com>
2024-05-18 17:38:36 +0530

    ui: Implement right panel

--- src/components/home/right-panel.tsx
+++ src/components/home/right-panel.tsx
@@ -1,8 +1,48 @@
+"use client";
+import { Video, X } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import ChatPlaceholder from "./chat-placeholder";
+import MessageContainer from "./message-container";
+import MessageInput from "./message-input";
 
 const RightPanel = () => {
+    const selectedConversation = null;
+    if(!selectedConversation) 
+        return <ChatPlaceholder />
+    
+    const conversationsName = "John Doe";
     return (
-        <div>
-            RightPanel
+        <div className="w-3/4 flex flex-col">
+            <div className="w-full sticky top-0 z-50">
+                {/* Header */}
+                <div>
+                    <div className="flex justify-between items-center bg-gray-800 p-5">
+                        <div className="flex items-center gap-5">
+                            <Avatar>
+                                <AvatarImage src={"/placeholder.png"} className="object-cover" />
+                                <AvatarFallback>
+                                <div className="animate-pulse w-full rounded-full" />
+                                </AvatarFallback>
+                            </Avatar>
+                            <div className="flex flex-col">
+                                <p>{conversationsName}</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div className="flex items-center gap-7 mr-5">
+                        <a href="/video-call" target="_blank">
+                            <Video size={23} />
+                        </a>
+                        <X size={16} className="cursor-pointer" />
+                    </div>
+                </div>
+            </div>
+            {/* Chat Messages */}
+            <MessageContainer />
+
+            {/* Input */}
+            <MessageInput />
         </div>
     )
 }
