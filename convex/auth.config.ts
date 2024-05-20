8cdc001daedf11817d9ff46c2a9b69b97f15a966
rathoretaruncodes <rathoretaruncodes@gmail.com>
2024-05-20 20:13:13 +0530

    feat: Implement clerk authentication

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+const authConfig = {
+    providers: [
+        {
+            domain: "https://true-martin-55.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
+
+export default authConfig;
