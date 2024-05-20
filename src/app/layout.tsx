9695cfc31635580b80c5b881d23c37ca0b81164e
rathoretaruncodes <rathoretaruncodes@gmail.com>
2024-05-20 14:57:26 +0530

    feat:Implement client component for convex provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
+import ConvexClientProvider from "@/providers/convex-client-provider";
 
 const font = Raleway({ subsets: ["latin"] });
 
@@ -19,7 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
       <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
-	      {children}
+      <ConvexClientProvider>
+        {children}
+      </ConvexClientProvider>
       </ThemeProvider>
       </body>
     </html>
