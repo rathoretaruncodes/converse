a9b21a2d1a1c5d716074cdb007ae35f2115d7ae8
rathoretaruncodes <rathoretaruncodes@gmail.com>
2024-05-21 14:00:54 +0530

    feat:Remove Convex schema created for initial testing purposes

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,13 +1,4 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
-export default defineSchema({
-    tasks: defineTable({
-        text: v.string(),
-        completed: v.boolean(),
-    }),
-    products: defineTable({
-        name: v.string(),
-        price: v.number(),
-    }),
-})
+export default defineSchema({})
