001c0f517c0eeb3d66e659fab890c916da162322
rathoretaruncodes <rathoretaruncodes@gmail.com>
2024-05-22 17:35:03 +0530

    feat: Implement webhooks for user login events

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,4 +1,12 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
-export default defineSchema({})
+export default defineSchema({
+    users: defineTable({
+        name: v.optional(v.string()),
+        email: v.string(),
+        image: v.string(),
+        tokenIdentifier: v.string(),
+        isOnline: v.boolean(),
+    }),
+});
