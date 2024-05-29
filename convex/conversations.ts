7b831157ac12e024bf3d80499317d13b7b5e5719
rathoretaruncodes <rathoretaruncodes@gmail.com>
2024-05-29 18:36:58 +0530

    feat: Introduce query for returning user specific conversations

--- convex/conversations.ts
+++ convex/conversations.ts
@@ -1,5 +1,5 @@
 import { ConvexError, v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 
 export const createConversation = mutation({
@@ -40,6 +40,30 @@ export const createConversation = mutation({
             admin: args.admin
         })
         return conversationId;
+    }
+})
+
+export const getMyConversations = query({
+    args: {},
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if(!identity) 
+            throw new ConvexError("Unauthorized");
+
+        const user = await ctx.db
+        .query("users")
+        .withIndex("by_tokenIdentifier", q=> q.eq("tokenIdentifier", identity.tokenIdentifier))
+        .unique();
+
+        if(!user)
+            throw new ConvexError("User not found.");
+
+        const conversations = await ctx.db.query("conversations").collect();
+
+        const myConversations = conversations.filter((conversation) => {
+            return conversation.participants.includes(user._id);
+        });
+        return myConversations;
     }
 })
 
