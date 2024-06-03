d0d3ecd874050a4019272b29e2c8b9a0d67b6b7f
rathoretaruncodes <rathoretaruncodes@gmail.com>
2024-06-03 18:54:18 +0530

    feat: Introduce 'getMessages query to retrieve previous messages

--- convex/messages.ts
+++ convex/messages.ts
@@ -1,5 +1,5 @@
 import { ConvexError, v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 
 export const sendTextMessage = mutation({
@@ -46,3 +46,32 @@ export const sendTextMessage = mutation({
         // TODO: Add chatGPT check.
     },
 });
+
+
+export const getMessages = query({
+    args: {
+        conversation: v.id("conversations"),
+    },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if (!identity) {
+            throw new ConvexError("Not authenticated");
+        }
+        const messages = await ctx.db
+        .query("messages")
+        .withIndex("by_conversation", q => q.eq("conversation", args.conversation))
+        .collect();
+
+        const messagesWithSender = await Promise.all(
+            messages.map(async (message) => {
+                const sender = await ctx.db
+                .query("users")
+                .filter(q => q.eq(q.field("_id"), message.sender))
+                .first();
+
+                return {...message, sender};
+            })
+        )
+        return messagesWithSender;
+    }
+})
